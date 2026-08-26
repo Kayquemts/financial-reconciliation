64486d81df7472936df8988d4b783f5310a484ed
Kayquemts <kayquemts@gmail.com>
2026-08-26 10:28:29 -0300

    feat: configure PostgreSQL connection, order entity, and repository

---
+++ src/main/java/com/kayque/financial_reconciliation/domain/Order.java
@@ -0,0 +1,28 @@
+package com.kayque.financial_reconciliation.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    private String id;
+
+    @Column(name = "order_amount", nullable = false)
+    private BigDecimal order_amount;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime dataCriacao;
+}
